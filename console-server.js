12c16886c107ce159106bcbb7e42a1f47f6b8c33
Gregg Stelmach <gregg@stelmach.net>
2025-05-23 20:44:48 -0700

    Update console-server.js

--- console-server.js
+++ console-server.js
@@ -1,23 +1,31 @@
+// Alternates between 200 and 500 responses. 
+
 http = require('http')
 fs = require('fs')
-
+let counter = 0;
 server = http.createServer( function(req, res) {
-
+    counter++;
+    console.log(counter)
     console.log('Method: ' + req.method)
     console.log('URL: ' + req.url)
     var body = ''
     req.on('data', function (data) {
       body += data
     })
-    req.on('end', function () {
-      console.log("Body: " + body)
-      res.writeHead(200, {'Content-Type': 'text/html'})
-      res.end()
-      console.log("======================")
+    req.on('end', function() {
+        console.log("Request Body: '" + body + "'")
+
+        res.setHeader('Content-Type', 'text/plain');
+        if (counter % 2 === 0) {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+        } else {
+            res.statusCode = 200;
+            res.end('OK');
+        }
+        console.log("statusCode: " + res.statusCode)
     })
-
 });
-
 port = process.argv[3] || 3000
 host = process.argv[2] || 'localhost'
 server.listen(port, host)
